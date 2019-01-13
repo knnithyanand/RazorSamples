e7fbe7206ad40046a3ab4448022d7048df7de086
Nithyanand K N <knnithyanand@hotmail.com>
2019-01-13 11:01:15 +0800

    Initial Commit

---
+++ src/ViewComponentBasic/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Knnithyanand.RazorSamples.ViewComponent.Basic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Knnithyanand.RazorSamples.ViewComponent.Basic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
