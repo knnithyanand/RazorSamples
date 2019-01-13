af9aa893ac54b10d12256687195cc9a173ea0743
Nithyanand K N <knnithyanand@hotmail.com>
2019-01-13 15:17:21 +0800

    Remove hint from PasswordField view model

--- src/ViewComponentBasic/Views/Shared/Components/PasswordField/PasswordFieldViewModel.cs
+++ src/ViewComponentBasic/Views/Shared/Components/PasswordField/PasswordFieldViewModel.cs
@@ -5,6 +5,5 @@ namespace Knnithyanand.RazorSamples.ViewComponentBasic.Views.Shared.Components
         public string Id { get; set; }
         public string Label { get; set; }
         public string Placeholder { get; set; }
-        public string Hint { get; set; }
     }
 }
