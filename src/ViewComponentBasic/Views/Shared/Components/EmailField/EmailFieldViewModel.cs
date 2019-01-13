31679c42045af70133708a8ee876c2cfb315763e
Nithyanand K N <knnithyanand@hotmail.com>
2019-01-13 14:47:56 +0800

    Add EmailField

---
+++ src/ViewComponentBasic/Views/Shared/Components/EmailField/EmailFieldViewModel.cs
@@ -0,0 +1,10 @@
+namespace Knnithyanand.RazorSamples.ViewComponentBasic.Views.Shared.Components
+{
+    public class EmailFieldViewModel
+    {
+        public string Id { get; set; }
+        public string Label { get; set; }
+        public string Placeholder { get; set; }
+        public string Hint { get; set; }
+    }
+}
