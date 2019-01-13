2930d6fcf186ac68832a8e272da06eaac8c4316d
Nithyanand K N <knnithyanand@hotmail.com>
2019-01-13 15:23:48 +0800

    Add CheckboxField

---
+++ src/ViewComponentBasic/Views/Shared/Components/CheckboxField/CheckboxFieldViewModel.cs
@@ -0,0 +1,8 @@
+namespace Knnithyanand.RazorSamples.ViewComponentBasic.Views.Shared.Components
+{
+    public class CheckboxFieldViewModel
+    {
+        public string Id { get; set; }
+        public string Label { get; set; }
+    }
+}
