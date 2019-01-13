afe44c136c0d480b77bf6c11a8874b9d92038d5b
Nithyanand K N <knnithyanand@hotmail.com>
2019-01-13 13:09:43 +0800

    Update project namespace

--- src/ViewComponentBasic/Program.cs
+++ src/ViewComponentBasic/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Knnithyanand.RazorSamples.ViewComponent.Basic
+namespace Knnithyanand.RazorSamples.ViewComponentBasic
 {
     public class Program
     {
